ec60903b92b4d104be7db1a6357e185faea4e259
Shantanu Mane <snake_eyes0017@yahoo.com>
2018-07-12 21:23:24 -0600

    Created Unreal Project

---
+++ ActionGameProject/Source/ActionGameProject/ActionGameProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionGameProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONGAMEPROJECT_API AActionGameProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
